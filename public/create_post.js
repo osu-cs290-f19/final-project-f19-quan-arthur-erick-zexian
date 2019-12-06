505321bf1aacc3ec0a868ccc0ff664b1744b6957
Erick Branner <brannere@flip1.engr.oregonstate.edu>
2019-12-06 14:30:03 -0800

    CSS is close. New post data stored in object on the client side.

--- public/create_post.js
+++ public/create_post.js
@@ -7,11 +7,12 @@
 
 
 /* Variables */
-
 const NUM_FIELDS = 10;
 const OPTIONAL_IDX = 6;
 const IPT_CLASS = 'create-input';
+const POST_TYPE = 'textbook'
 
+var reqISBN = "I-1234567";
 var postInfo;
 var postInfoArr = new Array(NUM_FIELDS);
 
@@ -19,7 +20,7 @@ var postInfoArr = new Array(NUM_FIELDS);
 /* Functions */
 
 function allFields(){
-    for(var i = 0; i < NUM_FIELDS; i++){
+    for(var i = 0; i < NUM_FIELDS-1; i++){
         if(i != OPTIONAL_IDX){
             if(document.getElementsByClassName(IPT_CLASS)[i].value.toString() == ""){
                 return false;
@@ -32,13 +33,36 @@ function allFields(){
 
 function storeInput(){
     postInfo = document.getElementsByClassName(IPT_CLASS);
-    for(var i = 0; i < NUM_FIELDS; i++){
-        postInfoArr[i] = postInfo[i];
+    for(var i = 0; i < NUM_FIELDS-1; i++){
+        postInfoArr[i] = postInfo[i].value;
     }
+    postInfoArr[NUM_FIELDS-1] = reqISBN;
     console.log(postInfoArr);
     console.log('== Input stored in postInfoArr');
-    return;
+    var newPost = {
+        "title": postInfoArr[0],
+        "author": postInfoArr[1],
+        "condition": postInfoArr[2],
+        "price": postInfoArr[3],
+        "meetupInfo": postInfoArr[4],
+        "description": postInfoArr[5],
+        "email": postInfoArr[6],
+        "phone": postInfoArr[7],
+        "imgURL": postInfoArr[8],
+        "isbn": reqISBN, 
+        "count": 0
+    };
+    console.log("== newPost object: ", newPost);
+    axios.post('/createPost', newPost)
+    .then(function (response){
+        console.log(response)
+    })
+    .catch(function (error){
+        console.log(error);
+    });    
+    return;     
 }
+
 
 
 /* Event Listeners */
@@ -55,7 +79,6 @@ createButton.addEventListener('click', () =>{
         alert('Please provide input for all fields');
     }
     else{
-        // console.log('== Store input data')
         storeInput();
     }
 
