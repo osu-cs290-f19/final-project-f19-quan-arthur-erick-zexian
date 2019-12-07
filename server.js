22d5a424a3415f3282dbf04cc1741ae1a74f121f
Quan Nguyen <45527447+QuanCfc@users.noreply.github.com>
2019-12-07 15:25:07 -0800

    Deleted textbookData.json (test file)

--- server.js
+++ server.js
@@ -69,16 +69,6 @@ app.get('/search/:course', async (req, res) => {
 
 // Testing Area
 
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, () => console.log('server is running on port 3000..'));
 
 // Get access token
