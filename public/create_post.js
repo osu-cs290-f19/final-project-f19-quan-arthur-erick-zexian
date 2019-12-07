6deea4fdff0c8eddaf6aac28106f8d0f2507534d
Erick Branner <brannere@oregonstate.edu>
2019-12-07 00:23:48 -0800

    Create post handlebars compiled into seperate file.

--- public/create_post.js
+++ public/create_post.js
@@ -1,6 +1,85 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['create_post'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "  <script src=\"https://unpkg.com/axios/dist/axios.min.js\"></script>\r\n            <script src=\"create_post.js\" defer></script>\r\n            <title>Create New Post | Textbook Buddy</title>\r\n            <main class=\"main\">\r\n                <div class = \"create-post-container\">\r\n                    <h1 id=\"course-name\" class=\"course-name\"> BI 579 - Introduction to Cats</h1>\r\n                </div>\r\n                \r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-title\" class=\"create-input-label\">Title</label>\r\n                        <div class=\"create-post-element\">\r\n                            <input type=\"text\" name=\"title-text\" id=\"title-text\" class=\"create-input\">\r\n                        </div>\r\n                </div>\r\n\r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-author\" class=\"create-input-label\">Author</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"text\" name=\"author-text\" id=\"author-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n\r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-condition\" class=\"create-input-label\">Condition</label>\r\n                    <div class=\"create-post-element\">\r\n                      <select id=\"create-condition\" class=\"create-input\" name=\"create-condition\">\r\n                        <option selected value=\"\"></option>\r\n                        <option>New</option>\r\n                        <option>Excellent</option>\r\n                        <option>Good</option>\r\n                        <option>Fair</option>\r\n                        <option>Poor</option>\r\n                      </select>\r\n                    </div>\r\n                </div>\r\n                                    \r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-price\" class=\"create-input-label\">Price</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"number\" name=\"price-text\" id=\"price-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n\r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-meet\" class=\"create-input-label\">Meetup Info</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"text\" name=\"meet-text\" id=\"meet-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n\r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-description\" class=\"create-input-label\">Description</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"text\" name=\"description-text\" id=\"description-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n                \r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-email\" class=\"create-input-label\">Email</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"text\" name=\"description-text\" id=\"email-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n\r\n                \r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-description\" class=\"create-input-label\">Phone</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"number\" name=\"phone-text\" id=\"phone-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n\r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-image\" class=\"create-input-label\">Image url</label>\r\n                    <div class=\"create-post-element\">\r\n                        <input type=\"text\" name=\"image-text\" id=\"image-text\" class=\"create-input\">\r\n                    </div>\r\n                </div>\r\n\r\n                <div class = \"create-post-container\">\r\n                    <label for=\"create-isbn\" class=\"create-input-label\">ISBN</label>\r\n                    <div class=\"create-post-element\">\r\n                        <label name=\"isbn-text\" id=\"isbn-text\" class=\"create-input\">I-1234567</label>\r\n                    </div>\r\n                </div>\r\n                \r\n            </div>\r\n                <div class = \"create-post-container\">\r\n                    <div class=\"create-input-footer\">\r\n                        <button type=\"button\" id=\"create-back\" class=\"btn primary-color\">Go Back</button>\r\n                        <button type=\"button\" id=\"create-accept\" class=\"btn primary-color\">Create</button>\r\n                    </div>\r\n                </div>\r\n\r\n            </main>\r\n\r\n        </body>\r\n    </head>\r\n</html>";
-},"useData":true});
-})();
+
+
+/***************
+ * JavaScript for create_post page
+ * Erick Branner
+ ***************/
+
+
+/* Variables */
+const NUM_FIELDS = 10;
+const OPTIONAL_IDX = 6;
+const IPT_CLASS = 'create-input';
+const POST_TYPE = 'textbook'
+
+var reqISBN = "155";
+var postInfo;
+var postInfoArr = new Array(NUM_FIELDS);
+
+
+/* Functions */
+
+function allFields(){
+    for(var i = 0; i < NUM_FIELDS-1; i++){
+        if(i != OPTIONAL_IDX){
+            if(document.getElementsByClassName(IPT_CLASS)[i].value.toString() == ""){
+                return false;
+            }
+        }
+    }
+    return true; 
+}
+
+
+function storeInput(){
+    postInfo = document.getElementsByClassName(IPT_CLASS);
+    for(var i = 0; i < NUM_FIELDS-1; i++){
+        postInfoArr[i] = postInfo[i].value;
+    }
+    postInfoArr[NUM_FIELDS-1] = reqISBN;
+    console.log(postInfoArr);
+    console.log('== Input stored in postInfoArr');
+    var newPost = {
+        "title": postInfoArr[0],
+        "author": postInfoArr[1],
+        "condition": postInfoArr[2],
+        "price": postInfoArr[3],
+        "meetupInfo": postInfoArr[4],
+        "description": postInfoArr[5],
+        "email": postInfoArr[6],
+        "phone": postInfoArr[7],
+        "imgURL": postInfoArr[8],
+        "isbn": reqISBN, 
+        "count": 0
+    };
+    console.log("== newPost object: ", newPost);
+    axios.post('/createPost', newPost)
+    .then(function (response){
+        console.log(response)
+    })
+    .catch(function (error){
+        console.log(error);
+    });    
+    return;     
+}
+
+
+
+/* Event Listeners */
+
+var goBack = document.getElementById('create-back');
+goBack.addEventListener('click', () =>{
+    console.log('== Go back clicked');
+});
+
+var createButton = document.getElementById('create-accept');
+createButton.addEventListener('click', () =>{
+    // console.log('== Create clcked');
+    if(!allFields()){
+        alert('Please provide input for all fields');
+    }
+    else{
+        storeInput();
+    }
+
+});
