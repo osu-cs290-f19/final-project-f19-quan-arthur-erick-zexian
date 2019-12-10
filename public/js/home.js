eea4254cf3d5c51d819253cacad69d26e386c0ea
Quan Nguyen <45527447+QuanCfc@users.noreply.github.com>
2019-12-10 12:53:09 -0800

    finished no-post page

--- public/js/home.js
+++ public/js/home.js
@@ -60,7 +60,7 @@ templates['book-result'] = template({"compiler":[8,">= 4.3.0"],"main":function(c
     + alias4(alias5(((stack1 = (depth0 != null ? depth0.attributes : depth0)) != null ? stack1.author : stack1), depth0))
     + "</p>\r\n      <p>ISBN: "
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data,"loc":{"start":{"line":12,"column":15},"end":{"line":12,"column":21}}}) : helper)))
-    + "</p>\r\n    </div>\r\n  </div>\r\n  <div id=\"sell-button\">\r\n    <input type=\"button\" value=\"Sell This Book\" class=\"btn primary-color sell-btn\">\r\n  </div>\r\n</div>";
+    + "</p>\r\n    </div>\r\n  </div>\r\n\r\n</div>";
 },"useData":true});
 templates['create_post'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression;
