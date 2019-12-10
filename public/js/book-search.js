93797db69c5250bdde75118ee58ef9f60e0413e3
Quan Nguyen <45527447+QuanCfc@users.noreply.github.com>
2019-12-09 19:40:03 -0800

    added eventListeners to all sell buttons and titles

--- public/js/book-search.js
+++ public/js/book-search.js
@@ -1,6 +1,17 @@
 const sellBtn = document.getElementsByClassName('sell-btn');
+const title = document.getElementsByClassName('title');
 
-sellBtn.addEventListener('click', (event) => {
-	const isbn = event.target.parentNode.parentNode.dataset.isbn;
-	window.location = '/create-post/' + isbn + '/';
-});
+for (i = 0; i < sellBtn.length; i++) {
+	sellBtn[i].addEventListener('click', (event) => {
+		const isbn = event.target.parentNode.parentNode.dataset.isbn;
+		window.location = '/create-post/' + isbn + '/';
+	});
+}
+
+for (i = 0; i < title.length; i++) {
+	title[i].addEventListener('click', (event) => {
+		const post = event.target.parentNode.parentNode.parentNode.parentNode;
+		const isbn = event.target.parentNode.parentNode.parentNode.parentNode.dataset.isbn;
+		window.location = '/details/' + isbn + '/';
+	});
+}
